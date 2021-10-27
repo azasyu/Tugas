ebf65b25fc5874cc7477fcbfb49ac8d5816e312a
azasyu <azaliasyuanidha.2020@students.amikom.ac.id>
2021-10-27 18:47:29 +0700

    Add project files.

---
+++ UnitLibrary/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace UnitLibrary
+{
+    [TestFixture]
+    public class Class1
+    {
+        private Day day;
+        private User user;
+
+        [SetUp]
+        public void Init()
+        {
+            day = new Day();
+            user = new User();
+        }
+        [Test]
+        public void DayTest()
+        {
+            Assert.AreEqual("Senin", day.NameOfDay(1));
+        }
+
+        [Test]
+        public void UserTest()
+        {
+            Assert.AreEqual(true, user.IsValidUser("admin", "admin"));
+        }
+
+    }
+}
